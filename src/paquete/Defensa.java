d99a4479dbb44713c4309e000af9a59849d1c6d4
Diego Cruz <dfcm950@gmail.com>
2023-08-18 14:09:33 -0600

    Creacion de clases de tipos de jugadores

---
+++ src/paquete/Defensa.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete;
+
+/**
+ *
+ * @author dfcm9
+ */
+public class Defensa extends Jugador{
+
+    public Defensa(String nombre, String nacionalidad, String pieh, int edad) {
+        super(nombre, nacionalidad, pieh, edad);
+        setRating();
+        setAgarre();
+        setLanzamiento();
+        setFisico();
+        setRitmo();
+        setEntrada();
+        setVision();
+        setPassing();
+        setRegate();
+        setDisparo();
+    }
+
+    @Override
+    public void setRating() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setAgarre() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setLanzamiento() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setFisico() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setRitmo() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setEntrada() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setVision() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setPassing() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setRegate() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void setDisparo() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
--- src/paquete/Jugador.java
+++ src/paquete/Jugador.java
@@ -1,3 +1,8 @@
+package paquete;
+
+
+import paquete.Equipo;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -7,28 +12,21 @@
  *
  * @author dfcm9
  */
-public class Jugador {
+public abstract class Jugador {
     
     private String nombre, nacionalidad, pieh;
     private Equipo equipo = new Equipo();
-    protected int edad, rating, agarre, lanzamiento, fisico, ritmo, entrada, vision, passing, regate, disparo;
+    private int edad;
+    protected int rating, agarre, lanzamiento, fisico, ritmo, entrada, vision, passing, regate, disparo;
 
-    public Jugador(String nombre, String nacionalidad, String pieh, int edad, int rating, int agarre, int lanzamiento, int fisico, int ritmo, int entrada, int vision, int passing, int regate, int disparo) {
+    public Jugador(String nombre, String nacionalidad, String pieh, int edad) {
         this.nombre = nombre;
         this.nacionalidad = nacionalidad;
         this.pieh = pieh;
         this.edad = edad;
-        this.rating = rating;
-        this.agarre = agarre;
-        this.lanzamiento = lanzamiento;
-        this.fisico = fisico;
-        this.ritmo = ritmo;
-        this.entrada = entrada;
-        this.vision = vision;
-        this.passing = passing;
-        this.regate = regate;
-        this.disparo = disparo;
     }
+
+    
 
     public Jugador() {
     }
@@ -77,86 +75,71 @@ public class Jugador {
         return rating;
     }
 
-    public void setRating(int rating) {
-        this.rating = rating;
-    }
+    public abstract void setRating();
 
     public int getAgarre() {
         return agarre;
     }
 
-    public void setAgarre(int agarre) {
-        this.agarre = agarre;
-    }
+    public abstract void setAgarre();
 
     public int getLanzamiento() {
         return lanzamiento;
     }
 
-    public void setLanzamiento(int lanzamiento) {
-        this.lanzamiento = lanzamiento;
-    }
+    public abstract void setLanzamiento();
 
     public int getFisico() {
         return fisico;
     }
 
-    public void setFisico(int fisico) {
-        this.fisico = fisico;
-    }
+    public abstract void setFisico();
 
     public int getRitmo() {
         return ritmo;
     }
 
-    public void setRitmo(int ritmo) {
-        this.ritmo = ritmo;
-    }
+    public abstract void setRitmo();
 
     public int getEntrada() {
         return entrada;
     }
 
-    public void setEntrada(int entrada) {
-        this.entrada = entrada;
-    }
+    public abstract void setEntrada();
 
     public int getVision() {
         return vision;
     }
 
-    public void setVision(int vision) {
-        this.vision = vision;
-    }
+    public abstract void setVision();
 
     public int getPassing() {
         return passing;
     }
 
-    public void setPassing(int passing) {
-        this.passing = passing;
-    }
+    public abstract void setPassing();
 
     public int getRegate() {
         return regate;
     }
 
-    public void setRegate(int regate) {
-        this.regate = regate;
-    }
+    public abstract void setRegate();
+       
+    
 
     public int getDisparo() {
         return disparo;
     }
 
-    public void setDisparo(int disparo) {
-        this.disparo = disparo;
-    }
+    public abstract void setDisparo();
 
     @Override
     public String toString() {
         return "Jugador: [Nombre: "+nombre+"]" + "[Nacionalidad: "+nacionalidad+"]" + "[Piehabil: "+pieh+"]" + "[Edad: "+edad+"]" ;
     }
+    
+    
+    
     
     
     
